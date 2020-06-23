2bda7e3dd3abd84a157056bcbb1f92d6070a6c3e
gitlab-ci-teguh <murdianto1561@gmail.com>
2020-06-23 04:36:35 +0000

    update latest pages

---
+++ component---src-pages-about-js-764c28112550fa43b4b7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"3XHS":function(n,t,e){"use strict";e.r(t);var u=e("q1tI"),o=e.n(u);t.default=function(n){return o.a.createElement("div",null,"About")}}}]);
+//# sourceMappingURL=component---src-pages-about-js-764c28112550fa43b4b7.js.map
